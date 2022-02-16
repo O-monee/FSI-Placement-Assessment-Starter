99c444777c6249d93dc298c4b67b609986358d8c
Otman <97642180+O-monee@users.noreply.github.com>
2022-02-16 18:30:30 -0500

    add event listener on JS file

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,73 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Otman" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 
-// selecting the element with an id of credit
-const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
+  //** Gingerbread (+), (-) btn
+let gbVote = document.querySelector('#qty-gb')
+// add event to plus btn
+let gbPlusBtn = document.querySelector('#add-gb');
 
-// Code to update name display
-credit.textContent = `Created by ${yourName}`
+ gbPlusBtn.addEventListener('click', function(e){
+     let gbPlusBtn = parseInt(gbVote.textContent);
+    gbPlusBtn = gbPlusBtn + 1;
+    gbVote.textContent = gbPlusBtn
+ 
+});
+// add event to Gingerbread minus btn
+let gbMinusBtn = document.querySelector('#minus-gb');
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+ gbMinusBtn.addEventListener('click', function(e){
+     let gbMinusBtn = parseInt(gbVote.textContent);
+    gbMinusBtn = gbMinusBtn - 1;
+    gbVote.textContent = gbMinusBtn
+ 
+});
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+// add event to CC (+), (-) btn.
+let ccVote = document.querySelector('#qty-cc')
+
+// cc minus btn
+let ccMinusBtn = document.querySelector('#minus-cc');
+
+ccMinusBtn.addEventListener('click', function(e) {
+    let ccMinusBtn = parseInt(ccVote.textContent);
+    ccMinusBtn = ccMinusBtn - 1;
+    ccVote.textContent = ccMinusBtn;
+})
+  
+// cc plus btn
+let ccPlusBtn = document.querySelector('#add-cc');
+
+ccPlusBtn.addEventListener('click', function(e) {
+    let ccPlusBtn = parseInt(ccVote.textContent);
+    ccPlusBtn = ccPlusBtn + 1;
+    ccVote.textContent = ccPlusBtn;
+})
+ 
+// add event to sugar (+), (-) btn.
+let sugarVote = document.querySelector('#qty-sugar')
+
+// Sugar minus btn
+let sugarMinusBtn = document.querySelector('#minus-sugar');
+
+sugarMinusBtn.addEventListener('click', function(e) {
+    let sugarMinusBtn = parseInt(sugarVote.textContent);
+    sugarMinusBtn = sugarMinusBtn - 1;
+    sugarVote.textContent = sugarMinusBtn;
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+// Sugar plus btn
+let sugarPlusBtn = document.querySelector('#add-sugar');
+
+sugarPlusBtn.addEventListener('click', function(e) {
+    let sugarPlusBtn = parseInt(sugarVote.textContent);
+    sugarPlusBtn = sugarPlusBtn + 1;
+    sugarVote.textContent = sugarPlusBtn;
+})
+
